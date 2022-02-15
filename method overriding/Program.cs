41aadf32acdc7dbaf75a6be257a41249b67c6640
tsidevhp001 <tsidevhp001@TSI-HP-001>
2022-02-15 15:36:36 +0530

    Add project files.

---
+++ method overriding/Program.cs
@@ -0,0 +1,56 @@
+﻿// C# program to illustrate the use of
+//'virtual' and 'override' modifiers
+using System;
+
+class baseClass
+{
+
+	// show() is 'virtual' here
+	public virtual void show()
+	{
+		Console.WriteLine("Base class");
+	}
+}
+
+
+// class 'baseClass' inherit
+// class 'derived'
+class derived : baseClass
+{
+
+	//'show()' is 'override' here
+	public override void show()
+	{
+		Console.WriteLine("Derived class");
+	}
+}
+
+class GFG
+{
+
+	// Main Method
+	public static void Main()
+	{
+
+		baseClass obj;
+
+		// 'obj' is the object
+		// of class 'baseClass'
+		obj = new baseClass();
+
+		// it invokes 'show()'
+		// of class 'baseClass'
+		obj.show();
+
+
+		// the same object 'obj' is now
+		// the object of class 'derived'
+		obj = new derived();
+
+		// it invokes 'show()' of class 'derived'
+		// 'show()' of class 'derived' is overridden
+		// for 'override' modifier
+		obj.show();
+
+	}
+}
